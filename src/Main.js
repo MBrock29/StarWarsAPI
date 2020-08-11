36fc8a717f455b23cffd0a78cde8efe5d309b817
MBrock29 <mbrock19@gmail.com>
2020-08-11 16:26:23 +0100

    fixing cors issue

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,9 @@ export default function Main() {
     }, 2000);
   }, [])
   useEffect(() => {
-  fetch("https://swapi.dev/api/people", {mode: "no-cors"})
+  fetch("https://swapi.dev/api/people", {mode: "no-cors", header: {
+    'Access-Control-Allow-Origin':'*',
+  }})
     .then((response) => response.json())
     .then((data) => setAPIData(data))
   }, [])
