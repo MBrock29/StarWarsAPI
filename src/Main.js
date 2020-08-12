194eb1e9956b1eb55e6e5660322002cdbfc878a9
MBrock29 <mbrock19@gmail.com>
2020-08-12 11:56:24 +0100

    Switching API

--- src/Main.js
+++ src/Main.js
@@ -10,9 +10,7 @@ export default function Main() {
     }, 2000);
   }, [])
   useEffect(() => {
-  fetch("https://swapi.dev/api/people", {mode: "no-cors", header: {
-    'Access-Control-Allow-Origin':'*',
-  }})
+  fetch("https://the-one-api.herokuapp.com/v1/book")
     .then((response) => response.json())
     .then((data) => setAPIData(data))
   }, [])
@@ -21,13 +19,13 @@ export default function Main() {
   if(loading === true) return <div><Loading /><h2>Loading...</h2></div>
   return(
     <div className="container">
-      {APIdata.results.map((item) => (
+      {APIdata.docs.map((item) => (
         <div className="zoom"><div className="cards"><h1  key={Math.random()}>{item.name}</h1>
-        <h2>Height:&nbsp;{item.height}cm</h2>
-        <h2>Weight:&nbsp;{item.mass}kg</h2>
+        {/* <h2>Height:&nbsp;{item.height}cm</h2>
+        <h2>Weight:&nbsp;{item.mass}kg</h2> */}
         </div></div>
       ))}
     </div>
   )
 }
-  
+
